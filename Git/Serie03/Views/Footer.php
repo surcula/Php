c8b489aab53232cd14dc7d4a543103f5ea2f41b6
david gaillet <david.gaillet@live.fr>
2021-10-03 16:43:53 +0200

    EX 03

---
+++ Git/Serie03/Views/Footer.php
@@ -0,0 +1,9 @@
+<footer class="col-lg-12">
+    Fait par LAWAREE Xavier
+    <a href="#haut" >haut de page</a>
+</footer>
+</div>
+
+</div>
+</body>
+</html>
---
+++ Git/série01/View/page1.php
@@ -0,0 +1,57 @@
+
+
+<div class="container-fluid">
+    <div class="row">
+        <header id="header" class="col-lg-10 offset-3">
+
+            <h1>Actu!</h1>
+        </header>
+    </div>
+</div>
+<div class="container">
+
+    <div class="row">
+        <section class="col-lg-10">
+            <div class="row">
+                <article class="col-sm-8">
+                    <img class="img-responsive" src="images/1.jpg" alt="">
+                    <p>Charles Michel avec les étudiants en Erasmus</p>
+
+                </article>
+                <article class="col-sm-4">
+                    <img class="img-responsive" src="images/2.jpg" alt="image cop23">
+                    <p>COP23:"La Belgique partage le sentiment d'urgence et agit</p>
+                </article>
+                <article class="col-sm-4">
+                    <img class="img-responsive" src="images/3.jpg" alt="hugo Clément">
+                    <p>Le journaliste Hugo Clément quitte «Quoditien»</p>
+                </article>
+
+                <article class="col-sm-4">
+                    <img class="img-responsive" src="images/4.jpg" alt="image sanglier coincé">
+                    <p>Un sanglier coincé dans une piscine à Erpent</p>
+                </article>
+                <article class="col-sm-8">
+                    <img class="img-responsive" src="images/5.jpg" alt="image d'un Rassemblement devant l’Albertine">
+                    <p>Rassemblement devant l’Albertine</p>
+                </article>
+
+
+            </div>
+        </section>
+        <aside class="col-lg-2">
+            <h2>Fil d'actualité</h2>
+            <ul>
+                <li>Aujourd'hui à 21:07 VIDÉO | Des feux d’artifice «silencieux» au nouvel an pour respecter les animaux?</li>
+                <li>Aujourd'hui à 21:04 Liban: le Premier ministre démissionnaire à Paris</li>
+                <li>Aujourd'hui à 21:02 Charles Michel avec des étudiants Erasmus en Suède</li>
+                <li>Aujourd'hui à 20:47 Accident avec un taxi: on recherche le témoin</li>
+                <li>Aujourd'hui à 20:26 Drame à Huccorgne: une maman et une fillette décédées</li>
+
+            </ul>
+        </aside>
+    </div>
+</div>
+
+<div class="container-fluid">
+    <div class="row">
